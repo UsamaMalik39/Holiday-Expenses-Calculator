b1622f86dd1ce97bdda0438f4a07bc4cd579fae9
Malik39 <usama.faisal@daoproptech.com>
2023-06-24 04:10:36 +0500

    removed redundancy

--- backend/routes/payouts.js
+++ backend/routes/payouts.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const calculator = require('../controllers/calculator')
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 router.post("/",
 calculator.calculateExpenses);
 
